49efe682babb2bf5be773374568ef9ee2f5e66c6
masterr <chguenter@web.de>
2013-09-23 17:10:28 +0200

    Euler 29 solved

---
+++ Euler_29.lisp
@@ -0,0 +1,17 @@
+(defun integer-cominations(a b)
+  (let ((comb nil))
+	  (mapc (lambda (x)
+	          (mapc (lambda (y)
+	            			(push (cons x y) comb))
+	            	(loop for i from a to b
+	                  		  collect i)))
+	   (loop for i from a to b
+	               collect i))
+   comb))
+
+(defun powers(list)
+	(mapcar (lambda (y)
+	  			(append (expt (car y) (cdr y))))
+	   		list))
+
+(print (length(remove-duplicates(sort (powers(integer-cominations 2 100)) #'<))))
